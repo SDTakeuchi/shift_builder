91f3ff3b6be33bd0a7c35ea5ef23a2b3c884428f
SDTakeuchi <kuranku191952996@gmail.com>
2023-02-08 22:08:07 +0900

    add input xlsx

--- worker.py
+++ worker.py
@@ -1,9 +1,11 @@
-class Employee():
-    def __init__(self, name, hour_total, desired_dayoffs, unavailable_days):
-        self.name: str = name
-        self.hour_total: int = hour_total
-        self.desired_dayoffs: list[int] = desired_dayoffs
-        self.unavailable_days: list[int] = unavailable_days
+from dataclasses import dataclass
 
-# 0:sunday ... 6:saturday
+@dataclass
+class Employee():
+    name: str
+    hour_total: int
+    desired_dayoffs: list[int]
+    unavailable_days: list[int]
+
+# 0: sunday ... 6: saturday
 week = [0, 1, 2, 3, 4, 5, 6]
