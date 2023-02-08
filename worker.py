00166fe93eb0527d78e9ef43ed89bef76b8b1df9
SDTakeuchi <kuranku191952996@gmail.com>
2023-02-08 21:12:47 +0900

    add memo

--- worker.py
+++ worker.py
@@ -1,6 +1,9 @@
 class Employee():
-    def __init__(self, name, hour_total, desired_dayoffs):
+    def __init__(self, name, hour_total, desired_dayoffs, unavailable_days):
         self.name: str = name
         self.hour_total: int = hour_total
         self.desired_dayoffs: list[int] = desired_dayoffs
-        
+        self.unavailable_days: list[int] = unavailable_days
+
+# 0:sunday ... 6:saturday
+week = [0, 1, 2, 3, 4, 5, 6]
